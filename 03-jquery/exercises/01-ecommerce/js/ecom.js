52b90840187ee81b23ebdda425afbc3aa3b9a23a
Marcos Baez <mbaezpy@gmail.com>
2015-10-06 15:08:46 +0200

    initial commit

--- 03-jquery/exercises/01-ecommerce/js/ecom.js
+++ 03-jquery/exercises/01-ecommerce/js/ecom.js
@@ -10,24 +10,10 @@ var tmpl = "<li>" +
 
 $( document ).ready(function() {
     
-     var fnAddProduct = function(product){
-       $(".items").append(tmpl.replace("TITLE", product));  
-     };
-  
-     $(".btn-add").click(function(){
-         var product = $("#product").val();
-         fnAddProduct(product);
-     });        
-
-    $( ".items" ).on( "click", "li", function(event) {
-      //$(event.currentTarget).remove();
-      $(event.currentTarget).addClass("selected");
-      
-    });  
   
     // we add elements dynamically 
     for(var i=0; i< products.length; i++){
-      fnAddProduct(products[i].name);
+      //fnAddProduct(products[i].name);
     }
 
 });
--- 03-jquery/exercises/02-triangle/js/app.js
+++ 03-jquery/exercises/02-triangle/js/app.js
@@ -1,29 +1,7 @@
 /* Main Try > file */
 
 $(document).ready(function(){  
-        
-    $(".results h1").hide();
-    $(".results h1.msg-default").show();
-    
-    $(".btn-verify").click(function(event){    
-        // the magic happens here. 
-        var a = parseInt($("#side_a").val());
-        var b = parseInt($("#side_b").val());
-        var c = parseInt($("#side_c").val());
-        
-        if (isNaN(a) || isNaN(b) || isNaN(c)){
-            $(".results h1").hide();
-            $(".results h1.msg-nodata").show();
-            return;
-        } 
-        
-        if (TryAngle.isTriangle(a, b, c)){
-            $(".results h1").hide();
-            $(".results h1.msg-notriangle").show();             return;
-        };
-        
-        
-    });        
+             
 
 });
 
