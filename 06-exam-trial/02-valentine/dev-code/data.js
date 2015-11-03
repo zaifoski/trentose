6cfbebc9948d93451e8fdbbd34a44510fccc030a
Marcos Baez <mbaezpy@gmail.com>
2015-11-03 16:12:22 +0100

    reorgnised the folder to include a new exam example

---
+++ 06-exam-trial/02-valentine/dev-code/data.js
@@ -0,0 +1,24 @@
+/* Don't modify this file */
+
+var data = [
+{
+ id : "theme1",
+ name : "See you tonight",
+ img : "img/theme1.jpg"
+}, 
+{
+ id : "theme2",
+ name : "Winter love",
+ img : "img/theme2.jpg"
+}, 
+{
+ id : "theme3",
+ name : "Geek love",
+ img : "img/theme3.jpg"
+}, 
+{
+ id : "theme4",
+ name : "Finger crossed",
+ img : "img/theme4.jpg"
+}                    
+];
