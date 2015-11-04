fd345f7b3494b8a67f62ee79ad74ebcfc28fa6de
zaifoski <sofiachimirri@gmailcom>
2015-11-04 21:43:45 +0100

    Add calories counter

--- 06-exam-trial/03-eat/dev-code/app.js
+++ 06-exam-trial/03-eat/dev-code/app.js
@@ -1,6 +1,26 @@
 /* your code should go here */
+var tmpl="<div> <img src=\"IMAGE\"><h2>NAME</h2><p>CAL calories</p><button id=\"NAMEL\" value=\"CAL\">Add</button></div>";
+
 $(document).ready(function(){
   
-  // you may want to write things here
-  
+    var tot = 0;
+    $(".nodrink").hide();
+    $(".added").hide();
+    
+    // we add elements dynamically 
+    for(var i=0; i< data.length; i++){
+      var item = tmpl.replace("IMAGE", data[i].image);
+      item = item.replace("NAME", data[i].name);
+      item = item.replace("NAMEL", (data[i].name).toLowerCase());
+      item = item.replace(/CAL/g, data[i].calories);
+      $(".food").append(item);
+    }
+    
+    var newTotal = 0;
+    
+    $("button").click(function(){
+        newTotal = parseInt(newTotal) + parseInt($(this).val());
+        $("#total").html(newTotal);
+    });
+    
 });
